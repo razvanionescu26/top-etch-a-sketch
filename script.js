796657814837085d4e47629fa7706a459dced160
Razvan Ionescu <44871305+razvanionescu26@users.noreply.github.com>
2026-06-29 23:34:00 +0300

    Create hover effect for grid

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function createGrid() {
+    
+    function changeGridColor(event) {
+        const target = event.target;
+        target.style.backgroundColor = "black";
+    }
+
     const gridSection = document.querySelector("#etch-a-sketch");
 
     for (let boxCount = 0; boxCount < 256; ++boxCount) {
         const squareDiv = document.createElement("div");
         squareDiv.classList.toggle("box");
+        squareDiv.addEventListener("mouseover", changeGridColor);
         gridSection.appendChild(squareDiv);
     }
 }
