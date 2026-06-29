e9eaa3f41af72f4e5da1f88e96aa15411635034a
Razvan Ionescu <44871305+razvanionescu26@users.noreply.github.com>
2026-06-30 00:09:11 +0300

    Implement reset button and refactor code

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 function createGrid() {
+    const gridSection = document.querySelector("#etch-a-sketch");
     
+    for (let boxCount = 0; boxCount < 256; ++boxCount) {
+        gridSection.appendChild(createGridBox());
+    }
+
+    function createGridBox() {
+        const squareDiv = document.createElement("div");
+        squareDiv.classList.toggle("box");
+        squareDiv.addEventListener("mouseover", changeGridColor);
+        return squareDiv;
+    }
+
     function changeGridColor(event) {
         const target = event.target;
         target.style.backgroundColor = "black";
     }
+}
 
-    const gridSection = document.querySelector("#etch-a-sketch");
+function makeResetButton() {
+    const resetButton = document.querySelector("#reset-button");
+    resetButton.addEventListener("click", resetGrid);
 
-    for (let boxCount = 0; boxCount < 256; ++boxCount) {
-        const squareDiv = document.createElement("div");
-        squareDiv.classList.toggle("box");
-        squareDiv.addEventListener("mouseover", changeGridColor);
-        gridSection.appendChild(squareDiv);
+    function resetGrid() {
+        const gridSection = document.querySelector("#etch-a-sketch");
+        const gridBoxes = gridSection.querySelectorAll(".box");
+        gridBoxes.forEach((box) => {box.style.backgroundColor = "white"});
     }
 }
 
+makeResetButton();
 createGrid();
