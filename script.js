f07e4dd28f4e5cd98cd9562b484c6a568b3d94eb
Razvan Ionescu <44871305+razvanionescu26@users.noreply.github.com>
2026-06-29 22:03:15 +0300

    Created grid of 16x16 boxes

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+function createGrid() {
+    const gridSection = document.querySelector("#etch-a-sketch");
+
+    for (let boxCount = 0; boxCount < 256; ++boxCount) {
+        const squareDiv = document.createElement("div");
+        squareDiv.classList.toggle("box");
+        gridSection.appendChild(squareDiv);
+    }
+}
+
+createGrid();
